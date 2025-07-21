96a85b7f87171f47b3715e8dc34cbd47c5db860d
Troy Neubauer <troyneubauer@gmail.com>
2025-07-20 21:16:16 -0700

    can open intent into settings app!

--- src/windows.rs
+++ src/windows.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use windows::{Devices::Geolocation::Geolocator, core::Result};
 use windows_future::IAsyncOperation;
 
@@ -6,6 +8,12 @@ pub fn go() {
     runtime
         .block_on(async move || -> Result<()> {
             let locator = Geolocator::new()?;
+
+            println!("Requesting access...");
+            if let Err(e) = Geolocator::RequestAccessAsync() {
+                println!("failed to get permissions: {e:?}");
+            }
+
             let pos = locator.GetGeopositionAsync()?.await?;
 
             let coord = pos.Coordinate()?;
@@ -15,6 +23,15 @@ pub fn go() {
             println!("Latitude:  {}", pos.Latitude);
             println!("Longitude: {}", pos.Longitude);
             println!("Altitude:  {} meters", pos.Altitude);
+
+            tokio::time::sleep(Duration::from_secs(5));
+
+            println!("Launching settings intent!");
+            let uri = windows::Foundation::Uri::CreateUri(&"ms-settings:privacy-location".into())?;
+
+            tokio::time::sleep(Duration::from_secs(1)).await;
+            windows::System::Launcher::LaunchUriAsync(&uri)?;
+            tokio::time::sleep(Duration::from_secs(1)).await;
 
             Ok(())
         }())
